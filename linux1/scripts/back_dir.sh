b24c6d01a6830d7238ab2ea3a9b109b601199e32
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-09-30 21:03:07 +0200

    Update

--- linux1/scripts/back_dir.sh
+++ linux1/scripts/back_dir.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# verifiera att ett argument har angetts.
-if test -z "$1" ; then
+# verifiera att ett argument har angetts
+if test -z "$1" ; then                                                              # test -z testar om en sträng är tom
     echo "Saknar argument. Ange en katalog för att skapa en säkerhetskopia." >&2
     exit 1
 fi
 
-# verifiera att argumentet är en existerande katalog.
-if test ! -d "$1"; then
+# verifiera att argumentet är en existerande katalog
+if test ! -d "$1"; then                                                             # test -d testar om en fil är en katalog
     echo "Argumentet är inte en existerande katalog." >&2
     exit 1
 fi
 
-# om en backup med namnet $1.bak redan finns, döp om den till $1.bak2. Befintlig $1.bak2 raderas om den existerar innan omdöpningen.
-if test -e "$1.bak"; then
-    if test -e "$1.bak2"; then
-        rm -r "$1.bak2"
+# om en backup med namnet $1.bak redan finns, döp om den till $1.bak2
+# gammal $1.bak2 raderas
+if test -e "$1.bak"; then                                                           # test -e testar om en fil existerar
+    if test -e "$1.bak2"; then 
+        rm -r "$1.bak2"                                                             # rm -r tar bort en katalog och dess innehåll
     fi
     mv "$1.bak" "$1.bak2"
 fi
 
-# skapa en säkerhetskopia av katalogen $1. Varje filnamn skrivs ut under tiden de kopieras
-cp -r "$1" "$1.bak" | while read -r line; do
+# skapa en säkerhetskopia av katalogen $1
+# varje filnamn skrivs ut under tiden de kopieras
+cp -rv "$1" "$1.bak" | while read -r line; do                     # -rv kopierar rekursivt och skriver ut filnamn |  while read -r line = läs in varje rad i $line
     echo "$line"
 done
---
+++ linux1/scripts/back_dirN.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# validera att minst ett argument har angivits
+if test $# -lt 1; then                                                                 # $# = antal argument  -lt = less than
+    echo "Saknar argument. Ange minst en katalog för att skapa en säkerhetskopia." >&2
+    exit 1
+fi
+
+# loopa igenom alla angivna argument och verifiera att de är existerande kataloger
+for dir in "$@"; do                                                                    # "$@" = alla argument
+    if test ! -d "$dir"; then                                                          # -d = directory
+        echo "Argumentet '$dir' är inte en existerande katalog." >&2
+        exit 1
+    fi
+done
+
+# loopa igenom alla angivna argument och skapa en backup för varje katalog
+for dir in "$@"; do
+    # om en backup med namnet $#.bak redan finns, döp om den till $#.bak2
+    # radera befintlig $#.bak2 om den existerar innan omdöpningen
+    if test -e "$dir.bak"; then                                                        # -e = existerar
+        if test -e "$dir.bak2"; then
+            rm -r "$dir.bak2"                                                          # -r = rekursivt
+        fi
+        mv "$dir.bak" "$dir.bak2"
+    fi
+
+    # skapa en ny backup av katalogen och namnge den till $#.bak
+    # varje filnamn skrivs ut under tiden de kopieras
+    cp -rv "$dir" "$dir.bak" | while read -r line; do                                  # -rv = rekursivt och verbose | while read -r line = läs in varje rad i $line
+        echo "$line"
+    done
+done
