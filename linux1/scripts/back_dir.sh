4f2196adb0c9629b6191a64f887b23b0c95f355d
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-09-30 21:05:14 +0200

    Update

--- linux1/scripts/back_dir.sh
+++ linux1/scripts/back_dir.sh
@@ -7,14 +7,14 @@ if test -z "$1" ; then                                                          
 fi
 
 # verifiera att argumentet är en existerande katalog
-if test ! -d "$1"; then                                                             # test -d testar om en fil är en katalog
+if test ! -d "$1"; then                                                             # test -d testar om det är en katalog
     echo "Argumentet är inte en existerande katalog." >&2
     exit 1
 fi
 
 # om en backup med namnet $1.bak redan finns, döp om den till $1.bak2
 # gammal $1.bak2 raderas
-if test -e "$1.bak"; then                                                           # test -e testar om en fil existerar
+if test -e "$1.bak"; then                                                           # test -e testar om den existerar
     if test -e "$1.bak2"; then 
         rm -r "$1.bak2"                                                             # rm -r tar bort en katalog och dess innehåll
     fi
