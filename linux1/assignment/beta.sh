4d51f00e27f9d0b5af62276513ce62fc96009f00
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-10-02 23:26:49 +0200

    Update - comments

--- linux1/assignment/beta.sh
+++ linux1/assignment/beta.sh
@@ -8,13 +8,15 @@ while IFS="," read -r firstname surname password operation; do                  
             echo "användaren $firstname$surname existerar redan" >&2                    # error print till stderr
             exit 1
         fi
-        echo "$operation" -m "$firstname$surname" -p "$password" | tee -a "$LOGG_FILE"  # skapa användare och logga utan att skriva över
+        # ta bort echo
+        echo useradd -m -p "$password" "$firstname$surname" | tee -a "$LOGG_FILE"       # skapa användare och logga utan att skriva över
     elif test "$operation" = "remove"; then                                             # kolla om användare ska raderas
         if ! id -u "$firstname$surname" >/dev/null; then                                # kolla om användaren finns, output discarded
             echo "användaren $firstname$surname existerar inte" >&2                     # error print til stderr
             exit 1
         fi
-        userdel -r "$firstname$surname" | tee -a "$LOGG_FILE"                           # radera användare och logga utan att skriva över
+        # ta bort echo
+        echo userdel -r "$firstname$surname" | tee -a "$LOGG_FILE"                      # radera användare och logga utan att skriva över
     else
         echo "okänt kommando: $operation använd add/remove" >&2
         exit 1
