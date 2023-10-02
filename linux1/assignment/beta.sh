c3534de0d8a083de00d2329cc5f524becd41fca4
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-10-02 21:48:22 +0200

    Update -  logg

--- linux1/assignment/beta.sh
+++ linux1/assignment/beta.sh
@@ -6,13 +6,13 @@ LOGG_FILE='logfile.txt'
 #    echo "$line"
 #done <"$MY_INPUT"
 
-while IFS="," read -r firstname surname password operation; do # IFS="," gör csv filen läsbar
+while IFS="," read -r firstname surname password operation; do # IFS="," gör csv-filen läsbar
     # om användaren redan finns
-    if id -u "$firstname$surname" >/dev/null 2>&1; then
-        echo "användaren $firstname$surname existerar redan" >&2
+    if id -u "$firstname$surname" >/dev/null 2>&1; then #  error print till stdout, discarded
+        echo "användaren $firstname$surname existerar redan" >&2 # error print till stderr
         exit 1
     fi
     # skapa användaren med lösenordet - ta bort echo
-    echo "$operation" -m "$firstname$surname" -p "$password"
+    echo "$operation" -m "$firstname$surname" -p "$password" | tee -a "$LOGG_FILE"
     # skippa första raden i csv-filen
 done < <(sed 1d "$MY_INPUT")
