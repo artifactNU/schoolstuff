7fa033a4fa656e1b54951b108df6d6108027264b
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-10-03 18:12:50 +0200

    Update - hash

--- linux1/assignment/beta.sh
+++ linux1/assignment/beta.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-if test $# -ne 1; then                                   # kontrollera att endast ett argument har angets & att det är en fil som existerar
-    echo "ange endast exakt en csv-fil som argument" >&2 # error print till stderr
+if test $# -ne 1; then                             # kontrollera att endast ett argument har angets & att det är en fil som existerar
+    echo "ange endast en csv-fil som argument" >&2 # error print till stderr
     exit 1
 fi
 
 MY_INPUT="$1" # user info csv-fil
 LOGG_FILE='logfile.txt'
 
-if test ! "$1" -f; then # kontrollera att argumentet är en vanlig fil
+if test ! "$1" -f; then                                # kontrollera att argumentet är en vanlig fil
     echo "argumentet består inte av en vanlig fil" >&2 # error print till stderr
     exit 1
 fi
 
 while IFS="," read -r firstname surname password operation; do       # IFS="," gör csv-filen läsbar
-    username="${firstname:0:3}${surname:0:3}"                        # definiera användarnamn
+    username="${firstname:0:3}${surname:0:3}"                        # definiera användarnamn & ta hänsyn på namn < 3 bokstäver
     if test "$operation" = "add"; then                               # kolla om användare ska skapas
         if id -u "$username" >/dev/null; then               # kolla om användaren finns, output discarded
             echo "användaren $username existerar redan" >&2 # error print till stderr
             exit 1
         fi
         # ta bort echo
-        echo useradd -m "$username" -p "$password" | tee -a "$LOGG_FILE" # skapa användare och logga utan att skriva över
+        echo useradd -m "$username" -p "$(mkpasswd "$password")" 2>&1 >/dev/null | tee -a "$LOGG_FILE" >&2 && echo "Generating hash for $username" >&2 # skapa användare, logga utan att skriva över & skapa lösenord hash
     elif test "$operation" = "remove"; then                              # kolla om användare ska raderas
         if ! id -u "$username" >/dev/null; then                          # kolla om användaren inte finns, output discarded
             echo "användaren $username existerar inte" >&2               # error print til stderr
