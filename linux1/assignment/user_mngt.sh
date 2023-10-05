69b9178930b58c5ac081b3d7b51197d6381ad4bd
artifactNU <simon@artifact.nu>
2023-10-05 09:21:50 +0200

    Update

--- linux1/assignment/user_mngt.sh
+++ linux1/assignment/user_mngt.sh
@@ -30,16 +30,16 @@ while IFS="," read -r firstname surname password operation; do # IFS="," gör cs
             echo "användaren $username existerar redan" >&2    # error print till stderr
             exit 1
         fi
-        echo "Skapar användare: $username" | tee -a "$LOGG_FILE"       #BONUS# logga skapande
-        useradd -m "$username" -p "$(mkpasswd "$password")" >/dev/null #BONUS# skapa användare med hashat lösenord & home directory, output discarded WIP generate hash comment
-        echo "Generating hash for $username" >&2                       #BONUS#
-    elif test "$operation" = "remove"; then                            #BONUS# kolla om användare ska raderas
-        if ! id -u "$username" 2>/dev/null; then                       #BONUS# kolla om användaren inte finns, output discarded
-            echo "användaren $username existerar inte" >&2             #BONUS# error print til stderr
+        echo "Skapar användare: $username" | tee -a "$LOGG_FILE"        #BONUS# logga skapande
+        useradd -m "$username" -p "$(mkpasswd "$password")" 2>/dev/null #BONUS# skapa användare med hashat lösenord & home directory, output discarded WIP generate hash comment
+        echo "Generating hash for $username" >&2                        #BONUS#
+    elif test "$operation" = "remove"; then                             #BONUS# kolla om användare ska raderas
+        if ! id -u "$username" 2>/dev/null; then                        #BONUS# kolla om användaren inte finns, output discarded
+            echo "användaren $username existerar inte" >&2              #BONUS# error print til stderr
             exit 1
         fi
         echo "Raderar användare: $username" | tee -a "$LOGG_FILE" #BONSU# logga radering
-        userdel -r "$username" >/dev/null                         #BONUS# radera användare & home directory, output discarded
+        userdel -r "$username" 2>/dev/null                        #BONUS# radera användare & home directory, output discarded
     else
         echo "okänt kommando: $operation använd add/remove" >&2 # error print till stderr
         exit 1
