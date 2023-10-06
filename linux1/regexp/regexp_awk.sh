02b49332e03ba2233f216710f2bcff4b2faa735b
artifactNU <simon@artifact.nu>
2023-10-06 14:46:58 +0200

    Update

--- linux1/regexp/regexp_awk.sh
+++ linux1/regexp/regexp_awk.sh
@@ -4,6 +4,13 @@
 #hela raden är $0
 #awk awk '{ print $0}' <"$1"
 
-#awk '/otta/ { print $0}' <"$1"
+#awk '/bob/ { print $0}' <"$1"
 
-awk 'END { print $ND}' <"$1"
+#awk 'END { print $ND}' <"$1"
+
+#skriv ut kolumn x
+#awk '{ print $2 }' "$1"
+
+#awk '/fish$/ { print $2 }' "$1"
+
+awk '/\/sh$/ { print $2 * 2 }' "$1"
--- linux1/regexp/regexp_sed.sh
+++ linux1/regexp/regexp_sed.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#https://regex101.com/
 
 sed -E 's/ +\././g' <fil.txt | sed -E 's/\./.\n/g' | sed -E 's/  +/ /g' | sed -E 's/^(-.+)\.$/B\1E/g'
