c8182f86fdb911db72b7536ad614f5e9502e5570
artifactNU <simon.nilsson.oberg@gmail.com>
2023-11-23 15:42:05 +0100

    Update

---
+++ WS/ADcsvImp.ps1
@@ -0,0 +1,27 @@
+# Specify the path for the CSV file
+$csvPath = "C:\Path\To\Import\Users.csv"
+
+# Specify the path to the target Organizational Unit
+$ouPath = "OU=Enskede,OU=Anvandare,DC=jultomten,DC=nu"
+
+try {
+    # Import user data from the CSV file
+    $userData = Import-Csv -Path $csvPath
+
+    # Create each user in Active Directory
+    foreach ($user in $userData) {
+        # Generate a SamAccountName and UserPrincipalName from the FirstName and Surname
+        $samAccountName = ($user.FirstName.ToLower() + "." + $user.Surname.ToLower()) -replace 'å', 'a' -replace 'ä', 'a' -replace 'ö', 'o'
+        $userPrincipalName = $samAccountName + "@jultomten.nu"
+
+        # Generate a random password (customize this logic)
+        $securePassword = ConvertTo-SecureString -AsPlainText "Linux4Ever" -Force
+
+        New-ADUser -SamAccountName $samAccountName -UserPrincipalName $userPrincipalName -Name ($user.FirstName + " " + $user.Surname) -GivenName $user.FirstName -Surname $user.Surname -Enabled $true -DisplayName ($user.FirstName + " " + $user.Surname) -Path $ouPath -AccountPassword $securePassword -City $user.City -Title $user.Title
+
+        Write-Host "User $samAccountName created"
+    }
+}
+catch {
+    Write-Host "An error occurred: $_"
+}
