869f527eb8248ed6b14cf3415cdd6e9897f418f3
artifactNU <simon.nilsson.oberg@gmail.com>
2023-11-27 12:15:21 +0100

    Update

--- WS/ADcsvImp.ps1
+++ WS/ADcsvImp.ps1
@@ -6,18 +6,31 @@ $ouPath = "OU=Anvandare,OU=Enskede,DC=jultomten,DC=nu"
 
 try {
     # Import user data from the CSV file
-    $userData = Import-Csv -Path $csvPath -Header FirstName, Surname, City, Title
+    $userData = Get-Content -Path $csvPath -Encoding UTF8 | Select-Object -Skip 1 | ConvertFrom-Csv -Header FirstName, Surname, City, Title
 
     # Create each user in Active Directory
     foreach ($user in $userData) {
         # Generate a SamAccountName and UserPrincipalName from the FirstName and Surname
-        $samAccountName = ($user.FirstName.ToLower() + "." + $user.Surname.ToLower()) -replace 'å', 'a' -replace 'ä', 'a' -replace 'ö', 'o'
+        $firstNameForLogon = $user.FirstName.ToLowerInvariant() -replace 'å', 'a' -replace 'ä', 'a' -replace 'ö', 'o' -replace 'Å', 'A' -replace 'Ä', 'A' -replace 'Ö', 'O'
+        $surnameForLogon = $user.Surname.ToLowerInvariant() -replace 'å', 'a' -replace 'ä', 'a' -replace 'ö', 'o' -replace 'Å', 'A' -replace 'Ä', 'A' -replace 'Ö', 'O'
+        $samAccountName = $firstNameForLogon + "." + $surnameForLogon
         $userPrincipalName = $samAccountName + "@jultomten.nu"
 
         # Generate a password
         $securePassword = ConvertTo-SecureString -AsPlainText "Linux4Ever" -Force
 
-        New-ADUser -SamAccountName $samAccountName -UserPrincipalName $userPrincipalName -Name ($user.FirstName + " " + $user.Surname) -GivenName $user.FirstName -Surname $user.Surname -Enabled $true -DisplayName ($user.FirstName + " " + $user.Surname) -Path $ouPath -AccountPassword $securePassword -City $user.City -Title $user.Title
+        # Create the user
+        New-ADUser -SamAccountName $samAccountName `
+                   -UserPrincipalName $userPrincipalName `
+                   -Name ($user.FirstName + " " + $user.Surname) `
+                   -GivenName $user.FirstName `
+                   -Surname $user.Surname `
+                   -Enabled $true `
+                   -DisplayName ($user.FirstName + " " + $user.Surname) `
+                   -Path $ouPath `
+                   -AccountPassword $securePassword `
+                   -City $user.City `
+                   -Title $user.Title
 
         Write-Host "User $samAccountName created"
     }
