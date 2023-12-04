2310027b33a4798f32757d251874e8137b5b53ab
artifactNU <simon.nilsson.oberg@gmail.com>
2023-12-04 13:19:48 +0100

    Create

---
+++ WS/ADgpoAllReport.ps1
@@ -0,0 +1,10 @@
+# Import the GroupPolicy module
+Import-Module GroupPolicy
+
+# Specify the path for the GPO report
+$gpoReportPath = "C:\GPOAllReport.html"
+
+# Generate the GPO report
+Get-GPOReport -All -ReportType Html -Path $gpoReportPath
+
+Write-Host "GPO report exported to $gpoReportPath"
---
+++ WS/ADgpoSpeReport.ps1
@@ -0,0 +1,13 @@
+# Import the GroupPolicy module
+Import-Module GroupPolicy
+
+# Specify the name of the GPO
+$gpoName = "Your GPO Name"
+
+# Specify the path for the GPO report
+$gpoReportPath = "C:\GPOSpecReport.html"
+
+# Generate the GPO report
+Get-GPOReport -Name $gpoName -ReportType Html -Path $gpoReportPath
+
+Write-Host "GPO report for $gpoName exported to $gpoReportPath"
