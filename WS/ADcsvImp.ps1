891e2e660c28326d13f6405146de98b656f3f3f5
artifactNU <simon.nilsson.oberg@gmail.com>
2023-12-06 09:51:01 +0100

    Update

--- WS/ADcsvImp.ps1
+++ WS/ADcsvImp.ps1
@@ -6,35 +6,41 @@ $ouPath = "OU=Anvandare,OU=Enskede,DC=jultomten,DC=nu"
 
 try {
     # Import user data from the CSV file
-    $userData = Get-Content -Path $csvPath -Encoding UTF8 | Select-Object -Skip 1 | ConvertFrom-Csv -Header FirstName, Surname, City, Title
+    $userData = Get-Content -Path $csvPath -Encoding UTF8 | Select-Object -Skip 1 | ConvertFrom-Csv -Header FirstName, Surname, PhoneNumber, Address, Postcode, City, Title
 
     # Create each user in Active Directory
     foreach ($user in $userData) {
         # Generate a SamAccountName and UserPrincipalName from the FirstName and Surname
-        $firstNameForLogon = $user.FirstName.ToLowerInvariant() -replace 'å', 'a' -replace 'ä', 'a' -replace 'ö', 'o' -replace 'Å', 'A' -replace 'Ä', 'A' -replace 'Ö', 'O'
-        $surnameForLogon = $user.Surname.ToLowerInvariant() -replace 'å', 'a' -replace 'ä', 'a' -replace 'ö', 'o' -replace 'Å', 'A' -replace 'Ä', 'A' -replace 'Ö', 'O'
-        $samAccountName = $firstNameForLogon + "." + $surnameForLogon
+        $firstNameForLogon = $user.FirstName.ToLowerInvariant() -replace 'å', 'a' -replace 'ä', 'a' -replace 'ö', 'o' -replace 'Å', 'A' -replace 'Ä', 'A' -replace 'Ö', 'O' -replace 'é', 'e' -replace 'É', 'E' -replace 'Ü', 'u'  -replace ' ', '.'
+        $surnameForLogon = $user.Surname.ToLowerInvariant() -replace 'å', 'a' -replace 'ä', 'a' -replace 'ö', 'o' -replace 'Å', 'A' -replace 'Ä', 'A' -replace 'Ö', 'O' -replace 'é', 'e' -replace 'É', 'E' -replace 'Ü', 'u'  -replace ' ', '.'
+        $samAccountName = ($firstNameForLogon + "." + ($surnameForLogon.Split(' ')[0])).Substring(0, [Math]::Min(20, ($firstNameForLogon + "." + ($surnameForLogon.Split(' ')[0])).Length))
+        $samAccountName = $samAccountName.TrimEnd('.') # Remove trailing dot
         $userPrincipalName = $samAccountName + "@jultomten.nu"
+
+        # Print the SamAccountName and UserPrincipalName to the console
+        Write-Host "SamAccountName: $samAccountName"
+        Write-Host "UserPrincipalName: $userPrincipalName"
 
         # Generate a password
         $securePassword = ConvertTo-SecureString -AsPlainText "Linux4Ever" -Force
 
-        # Create the user
-        New-ADUser -SamAccountName $samAccountName `
-                   -UserPrincipalName $userPrincipalName `
-                   -Name ($user.FirstName + " " + $user.Surname) `
-                   -GivenName $user.FirstName `
-                   -Surname $user.Surname `
-                   -Enabled $true `
-                   -DisplayName ($user.FirstName + " " + $user.Surname) `
-                   -Path $ouPath `
-                   -AccountPassword $securePassword `
-                   -City $user.City `
-                   -Title $user.Title
-
-        Write-Host "User $samAccountName created"
+# Create the user
+New-ADUser -SamAccountName $samAccountName `
+           -UserPrincipalName $userPrincipalName `
+           -Name ($user.FirstName + " " + $user.Surname) `
+           -GivenName $user.FirstName `
+           -Surname $user.Surname `
+           -Enabled $true `
+           -Title $user.Title `
+           -Description $user.Title `
+           -StreetAddress $user.Address `
+           -City $user.City `
+           -PostalCode $user.Postcode `
+           -OfficePhone $user.PhoneNumber `
+           -AccountPassword $securePassword `
+           -Path $ouPath
     }
 }
 catch {
-    Write-Host "An error occurred: $_"
+    Write-Error "An error occurred: $_"
 }
