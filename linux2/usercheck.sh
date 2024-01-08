120fdfd90f382605d1a86ddf84759507e20c5205
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2024-01-08 11:27:05 +0100

    Update

--- linux2/usercheck.sh
+++ linux2/usercheck.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "No argument provided. Please provide a username."
+  exit 1
+fi
+
 user=$1
 
 grep "$user" /etc/passwd
