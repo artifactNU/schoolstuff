d3b0a7b0f1bbf796f3fd9b8d97469a4b1b35bc4a
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2024-01-09 09:53:26 +0100

    Update

--- linux2/findsh.sh
+++ linux2/findsh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#find . -name "*sh" | while IFS= read -r file; do
-#  ls -l "$file"
-#done
-
-for i in $(find . -type f -name "*sh"); do
-  ls -l "$i"
+find . -name "*sh" | while IFS= read -r file; do
+  ls -l "$file"
 done
+
+#for i in $(find . -type f -name "*sh"); do
+#  ls -l "$i"
+#done
