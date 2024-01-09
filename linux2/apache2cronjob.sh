ce6114cce75b8e91fa28f44b9ca6ee0463687317
artifactNU <simon.nilsson.oberg@gmail.com>
2024-01-09 16:24:10 +0100

    Update

--- linux2/apache2cronjob.sh
+++ linux2/apache2cronjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$(systemctl is-active apache2)" == "active" ]; then
-    echo "Apache2 is running" >>/var/log/apache2/apache2cronjob.log
+    echo "Apache2 is running $(date)" >>/var/log/apache2/apache2cronjob.log
 else
-    echo "Apache2 is not running" >>/var/log/apache2/apache2cronjob.log
+    echo "Apache2 is not running $(date)" >>/var/log/apache2/apache2cronjob.log
 fi
