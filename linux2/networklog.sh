ecdc3bcce0eae3557b548b5f471d3686fce225a8
artifactNU <simon.nilsson.oberg@gmail.com>
2024-01-15 10:24:59 +0100

    Update

--- linux2/networklog.sh
+++ linux2/networklog.sh
@@ -3,9 +3,9 @@
 # Definiera funktionen för att kontrollera om ett gränssnitt är uppe
 check_interface() {
     if ping -I $1 -c 1 8.8.8.8 &>/dev/null; then
-        echo "$(date): $1 is up" >>networklog.txt
+        echo "$(date): $1 is up" >>/path/to/networklog.txt
     else
-        echo "$(date): $1 is down" >>networklog.txt
+        echo "$(date): $1 is down" >>/path/to/networklog.txt
     fi
 }
 
